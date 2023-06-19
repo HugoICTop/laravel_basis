65f0f548bc944edf6e0485dc831e77bd6e0f11c6
Hugo Viergever <hviergever@hotmail.com>
2023-06-19 22:40:06 +0200

    Update voor Max

--- resources/views/contactform.blade.php
+++ resources/views/contactform.blade.php
@@ -1,24 +1,24 @@
-
-
+<link rel="stylesheet" href="css/style.css" />
+<body>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <form method="POST" action="{{ route('demo.contact') }}">
                         @csrf
-                        Naam:
+                        <h3>Naam:</h3> 
                         <br />
                         <input type="text" name="naam"  >
                         <br /><br />
-                        Email:
+                        <h3>Email:</h3>   
                         <br />
                         <input type="text" name="email" >
                         <br /><br />
-                        Telefoon:
+                        <h3>Telefoon:</h3> 
                         <br />
                         <input type="text" name="telefoon" >
                         <br /><br />
-                        Message:
+                        <h3>Message:</h3>  
                         <br />
                         <input type="text" name="message" >
                         <br /><br />
@@ -28,3 +28,4 @@
             </div>
         </div>
     </div>
+</body>
